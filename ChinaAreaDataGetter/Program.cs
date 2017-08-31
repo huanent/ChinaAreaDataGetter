3e63fd858c9dbd65e17f66ef51f03208aa4139dc
李凤焕 <huanent@hotmail.com>
2017-08-31 14:12:41 +0800

    添加项目文件。

---
+++ ChinaAreaDataGetter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChinaAreaDataGetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
