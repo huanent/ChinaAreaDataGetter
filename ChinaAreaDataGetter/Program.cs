b4fef3b137f68a4926e5f3a7e8e5f8f5c3fbe396
李凤焕 <huanent@hotmail.com>
2018-02-09 15:40:45 +0800

    改用angelsharp库

--- ChinaAreaDataGetter/Program.cs
+++ ChinaAreaDataGetter/Program.cs
@@ -1,79 +1,78 @@
-﻿using Newtonsoft.Json;
+﻿using AngleSharp;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading;
 
 namespace ChinaAreaDataGetter
 {
     class Program
     {
-        static HttpClient httpClient = new HttpClient();
-        static string baseUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2016/";
+        static Uri baseUrl = new Uri("http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2016/");
 
+        static List<(string name, string code, string parentCode)> list = new List<(string name, string code, string parentCode)>();
         static void Main(string[] args)
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            string htmlStr = GetHtmlString(httpClient, $"{baseUrl}index.html");
-            var dataTemp = GetArea(htmlStr, null, null);
-            var data = dataTemp.Select(s => new Area
+            var config = Configuration.Default.WithDefaultLoader();
+            var provinces = GetProvinces(config).ToArray();
+
+            foreach (var (name, url, code) in provinces)
             {
-                Code = s.Code,
-                Name = s.Name,
-                Parent = s.Parent
-            });
-            string json = JsonConvert.SerializeObject(data);
-            File.WriteAllText("area.json", json);
-        }
-
-        private static string GetHtmlString(HttpClient httpClient, string url)
-        {
-            var html = httpClient.GetByteArrayAsync(url).Result;
-            return Encoding.GetEncoding("gb2312").GetString(html);
-        }
-
-        private static IList<AreaTemp> GetArea(string html, string path, string parentCode)
-        {
-            var list = new List<AreaTemp>();
-            var reg = new Regex(@"\d{2,}\.html'\s{0,}>\D{2,}?<");
-            var match = reg.Match(html);
-            while (true)
-            {
-                if (!match.Success) break;
-                string code = match.Value.Split('.')[0].Trim();
-                string name = match.Value.Split('>')[1].Split('<')[0].Trim();
-
-                var urlBuilder = new StringBuilder();
-                if (path != null) urlBuilder.Append($"{path}/");
-                string nextUrl = urlBuilder.Append($"{code}.html").ToString();
-                string padedCode = code.PadRight(12, '0');
-                list.Add(new AreaTemp
-                {
-                    Code = padedCode,
-                    Name = name,
-                    Parent = parentCode,
-                    NextUrl = nextUrl
-                });
-                Console.WriteLine($"{code},{name}");
-
-                try
-                {
-                    string nextHtml = GetHtmlString(httpClient, $"{baseUrl}{nextUrl}");
-                    var pathTemp = path == null ? "" : $"{path}/";
-                    var nextArea = GetArea(nextHtml, $"{pathTemp}{code.Substring(code.Length - 2)}", padedCode);
-                    list.AddRange(nextArea);
-                }
-                catch (AggregateException)
-                {
-                }
-
-                match = match.NextMatch();
+                AddCellsToList(config, url, code);
             }
-            return list;
+            Console.WriteLine("爬取完成");
+            string json = JsonConvert.SerializeObject(list);
+            File.WriteAllText("data.json", json);
+        }
+
+        /// <summary>
+        /// 递归添加数据到list
+        /// </summary>
+        /// <param name="config"></param>
+        /// <param name="parentUrl"></param>
+        /// <param name="parentCode"></param>
+        private static void AddCellsToList(IConfiguration config, Uri parentUrl, string parentCode)
+        {
+            var doc = BrowsingContext.New(config).OpenAsync(parentUrl.ToString()).Result;
+            var cells = doc.QuerySelectorAll(".citytr,.countytr,.towntr,.villagetr");
+            foreach (var item in cells)
+            {
+                string name = item.QuerySelectorAll("td").Last().TextContent;
+                string code = item.QuerySelector("td a")?.TextContent ?? item.QuerySelector("td").TextContent;
+                string url = item.QuerySelector("td a")?.GetAttribute("href");
+                Console.WriteLine($"{name} {url} {code}");
+                list.Add((name, code, parentCode));
+                if (url != null)
+                {
+                    string mUrl = Regex.Match(parentUrl.ToString(), ".+/").Value;
+                    AddCellsToList(config, new Uri(new Uri(mUrl), url), code);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 获取到省数据
+        /// </summary>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        private static IEnumerable<(string name, Uri url, string code)> GetProvinces(IConfiguration config)
+        {
+            var indexUrl = new Uri(baseUrl, "Index.html");
+            var doc = BrowsingContext.New(config).OpenAsync(indexUrl.ToString()).Result;
+            var provinces = doc.QuerySelectorAll(".provincetr a");
+            foreach (var item in provinces)
+            {
+                string name = item.TextContent;
+                string url = item.GetAttribute("href");
+                string code = Regex.Match(url, "[0-9].").Value;
+                Console.WriteLine($"{name} {url} {code}");
+                list.Add((name, code, ""));
+                yield return (name, new Uri(baseUrl, url), code);
+            }
         }
     }
 }
